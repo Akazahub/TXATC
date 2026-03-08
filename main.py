7fe9ad89003634a1fbfdc2591d6e20feadff3615
Akazahub <abderbekkarrahmane2009@gmail.com>
2026-03-08 22:09:02 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot is online: {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "!ping":
+        await message.channel.send("Pong!")
+
+TOKEN = os.getenv("TOKEN")
+client.run(TOKEN)
